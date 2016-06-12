87bfcd1b1efc4ed3f01b3deee405588664e20214
Vyacheslav Voronenko <git@voronenko.info>
2016-06-13 00:39:44 +0300

    initial commit

--- delete_a_record.sh
+++ delete_a_record.sh
@@ -9,7 +9,7 @@ APIPASS=${GODADDY_APISECRET}
 NAME=$1
 IP=$2
 TYPE=${3-A}
-DOMAIN=${4-voronenko.net}
+DOMAIN=${GODADDY_DOMAIN-voronenko.net}
 
 
 echo curl -H 'Content-Type: application/json' -H "Authorization: sso-key ${APIUSER}:${APIPASS}" -X PUT -d "[]" https://api.godaddy.com/v1/domains/${DOMAIN}/records/${TYPE}/${NAME}
--- update_a_record.sh
+++ update_a_record.sh
@@ -9,7 +9,7 @@ APIPASS=${GODADDY_APISECRET}
 NAME=$1
 IP=$2
 TYPE=${3-A}
-DOMAIN=${4-voronenko.net}
+DOMAIN=${GODADDY_DOMAIN-voronenko.net}
 
 
 echo curl -H 'Content-Type: application/json' -H "Authorization: sso-key ${APIUSER}:${APIPASS}" -X PUT -d "[{\"type\":\"${TYPE}\",\"name\":\"$NAME\",\"data\":\"$IP\",\"ttl\":3600}]" https://api.godaddy.com/v1/domains/${DOMAIN}/records
